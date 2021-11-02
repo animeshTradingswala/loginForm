30308f013b960d73893b4fffeb13672c01e514f8
USER <animesh.banerjee@tradingswala.com>
2021-11-02 05:00:17 -0700

    new

--- App.tsx
+++ App.tsx
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Alert } from 'react-native';
 
-export default function App() {
+const main = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Welcome here !</Text>
+      <View>
+      <Text style={styles.userIdText}>User Id</Text>
+      <TextInput style={styles.userIdTextField} placeholder="Enter your user-id"></TextInput>
+      <Text style={styles.passswordText}>Password</Text>
+      <TextInput style={styles.passwordTextField} placeholder="Enter your password"></TextInput>
+      </View>
+      <Button color='red'  onPress={() => Alert.alert('Simple Button pressed')} title='Submit' />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    padding:10,
+    flex:1,
+  },
+  heading:{
+    fontSize:30,
+    fontWeight:'bold',
+  },
+  userIdText:{
+    flexDirection: 'row',
     justifyContent: 'center',
   },
+  userIdTextField:{
+    borderColor:'black',
+    borderRadius:10,
+    borderWidth:1,
+    padding:10
+  },
+  passswordText:{},
+  passwordTextField:{
+    borderColor:'black',
+    borderRadius:10,
+    borderWidth:1,
+    padding:10,
+  },
+  button:{
+    // padding:20
+  },
+
 });
+
+
+export default main;
